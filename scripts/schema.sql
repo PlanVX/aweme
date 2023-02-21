fdede6e789a2d7576dcc4dfa162dfe2827ff6485
Restyled.io <commits@restyled.io>
2023-02-21 13:03:12 +0000

    Restyled by pg_format

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,67 +1,50 @@
-CREATE TABLE `videos`
-(
-    `id`         bigint(20) AUTO_INCREMENT,
-    `user_id`    bigint(20)   NOT NULL,
-    `video_url`  varchar(200) NOT NULL,
-    `cover_url`  varchar(200) NOT NULL,
-    `title`      varchar(200) NOT NULL,
-    `created_at` datetime(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE `videos` (
+    `id` bigint (20) AUTO_INCREMENT,
+    `user_id` bigint (20) NOT NULL,
+    `video_url` varchar(200) NOT NULL,
+    `cover_url` varchar(200) NOT NULL,
+    `title` varchar(200) NOT NULL,
+    `created_at` datetime (3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     PRIMARY KEY (`id`),
     KEY `created_at` (`created_at` DESC),
-    KEY `user_created` (`user_id`, `created_at` DESC)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    KEY `user_created` (`user_id`, `created_at` DESC)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-
-CREATE TABLE `users`
-(
-    `id`               bigint(20) AUTO_INCREMENT,
-    `username`         varchar(40)  NOT NULL,
-    `password`         varchar(200) NOT NULL,
-    `avatar`           varchar(200) DEFAULT NULL,
+CREATE TABLE `users` (
+    `id` bigint (20) AUTO_INCREMENT,
+    `username` varchar(40) NOT NULL,
+    `password` varchar(200) NOT NULL,
+    `avatar` varchar(200) DEFAULT NULL,
     `background_image` varchar(200) DEFAULT NULL,
-    `signature`        varchar(200) DEFAULT NULL,
+    `signature` varchar(200) DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `username` (`username`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    UNIQUE KEY `username` (`username`)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-
-CREATE TABLE `comments`
-(
-    `id`         bigint(20) AUTO_INCREMENT,
-    `content`    text        NOT NULL,
-    `video_id`   bigint(20)  NOT NULL,
-    `user_id`    bigint(20)  NOT NULL,
-    `created_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE `comments` (
+    `id` bigint (20) AUTO_INCREMENT,
+    `content` text NOT NULL,
+    `video_id` bigint (20) NOT NULL,
+    `user_id` bigint (20) NOT NULL,
+    `created_at` datetime (3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     PRIMARY KEY (`id`),
     KEY `video_index` (`video_id`, `created_at` DESC),
-    KEY `video_user_index` (`video_id`, `user_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    KEY `video_user_index` (`video_id`, `user_id`)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `likes`
-(
-    `id`         bigint(20) AUTO_INCREMENT,
-    `video_id`   bigint(20)  NOT NULL,
-    `user_id`    bigint(20)  NOT NULL,
-    `created_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-#     `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE `likes` (
+    `id` bigint (20) AUTO_INCREMENT,
+    `video_id` bigint (20) NOT NULL,
+    `user_id` bigint (20) NOT NULL,
+    `created_at` datetime (3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    # `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     PRIMARY KEY (`id`),
     UNIQUE KEY `unique_index` (`video_id`, `user_id`),
-    KEY `user_index` (`user_id`, `created_at` DESC, `video_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    KEY `user_index` (`user_id`, `created_at` DESC, `video_id`)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-
-CREATE TABLE `relations`
-(
-    `id`         bigint(20) AUTO_INCREMENT,
-    `user_id`    bigint(20) NOT NULL,
-    `follow_to`  bigint(20) NOT NULL,
-    `created_at` datetime   NOT NULL,
+CREATE TABLE `relations` (
+    `id` bigint (20) AUTO_INCREMENT,
+    `user_id` bigint (20) NOT NULL,
+    `follow_to` bigint (20) NOT NULL,
+    `created_at` datetime NOT NULL,
     PRIMARY KEY (`id`),
     KEY `user_created` (`user_id`, `created_at` DESC),
-    KEY `follow_to_created` (`follow_to`, `created_at` DESC)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    KEY `follow_to_created` (`follow_to`, `created_at` DESC)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
